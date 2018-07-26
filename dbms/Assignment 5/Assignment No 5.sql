0343b77265de0e895a7ee51402646a9015c838e1
unknown <ayushbansal323@gmail.com>
2018-07-26 07:22:33 +0530

    ass 5

--- dbms/Assignment 5/Assignment No 5.sql
+++ dbms/Assignment 5/Assignment No 5.sql
@@ -13,7 +13,6 @@ rollno number(20);
 BEGIN
 rollno:=&rollno;
 select ATTENDENCE into presenty from students where ROLL_NO=rollno;
-dbms_output.put_line('dance'||presenty);
 IF(presenty < 75) THEN
 update students set STATUS='D' where ROLL_NO=rollno;
 dbms_output.put_line('Term not granted'||presenty);
@@ -46,3 +45,66 @@ SQL> SELECT * FROM students
 */
 
 create table acc_master(acc_no number(20) primary key,balance number(20) not null,status VARCHAR(4));
+
+INSERT INTO acc_master (ACC_NO,BALANCE) values (1,10000);
+INSERT INTO acc_master (ACC_NO,BALANCE) values (2,20000);
+INSERT INTO acc_master (ACC_NO,BALANCE) values (3,30000);
+INSERT INTO acc_master (ACC_NO,BALANCE) values (4,40000);
+INSERT INTO acc_master (ACC_NO,BALANCE) values (5,50000);
+
+DECLARE
+accno number(20);
+withdraw number(20);
+bal number(20);
+cannot_withdraw exception;
+BEGIN
+withdraw:=&withdraw;
+accno:=&accno;
+select BALANCE into bal from acc_master where ACC_NO=accno;
+dbms_output.put_line('balance = '||bal);
+IF((bal-withdraw) < 0) THEN
+raise cannot_withdraw;
+ELSE
+update acc_master set BALANCE=(bal-withdraw) where ACC_NO=accno;
+dbms_output.put_line('balance deducted by = '||withdraw);
+END IF;
+EXCEPTION
+when cannot_withdraw then
+dbms_output.put_line('cannot withdraw = '||withdraw);
+END;
+
+/*
+Enter value for withdraw: 1000
+old   7: withdraw:=&withdraw;
+new   7: withdraw:=1000;
+Enter value for accno: 1
+old   8: accno:=&accno;
+new   8: accno:=1;
+dance9800
+balance deducted by = 1000
+
+PL/SQL procedure successfully completed.
+
+SQL> /
+Enter value for withdraw: 100000
+old   7: withdraw:=&withdraw;
+new   7: withdraw:=100000;
+Enter value for accno: 1
+old   8: accno:=&accno;
+new   8: accno:=1;
+dance8800
+cannot withdraw = 100000
+
+PL/SQL procedure successfully completed.
+*/
+
+
+
+
+
+
+
+
+
+
+
