6ba8778c94861481d921929982e12ea5117708f7
-ubuntu <ayushbansal323@gmail.com>
2019-01-18 13:06:17 +0530

    syscall p1.c p2.c

--- spos/syscall/p1.c
+++ spos/syscall/p1.c
@@ -9,7 +9,7 @@ int main()
 	int status = 80000;
 	while(1)
 	{
-		printf("********************************Menu***************************\n");
+		printf("\n\n********************************Menu***************************\n");
 		printf("1.fork \n2.exec \n3.wait \n4.ps \n5.joint \n6.exit\nEnter choice =>");
 	
 		scanf("%d",&op);
@@ -24,16 +24,19 @@ int main()
 					}
 					else
 					{
+						wait(&status);
 						printf("This is parent process pid => %d \n",getpid());
 					}
 					break;
 			case 2:
-					if(fork==0)
+					if(fork()==0)
 					{
+						
 						execv("./p2",0);
 					}
-					break;
 					wait(&status);
+					break;
+					
 			case 3:
 					wait(&status);
 					printf("After waiting for 80000msec\n");
@@ -54,7 +57,80 @@ int main()
 	}
 	return 0;
 }
+/*
 
+********************************Menu***************************
+1.fork 
+2.exec 
+3.wait 
+4.ps 
+5.joint 
+6.exit
+Enter choice =>1
+This is child process pid => 5809 
+This is parent process pid => 5808 
+
+
+********************************Menu***************************
+1.fork 
+2.exec 
+3.wait 
+4.ps 
+5.joint 
+6.exit
+Enter choice =>2
+
+
+This is process 2 ppid => 5808
+
+
+
+********************************Menu***************************
+1.fork 
+2.exec 
+3.wait 
+4.ps 
+5.joint 
+6.exit
+Enter choice =>3
+After waiting for 80000msec
+
+
+********************************Menu***************************
+1.fork 
+2.exec 
+3.wait 
+4.ps 
+5.joint 
+6.exit
+Enter choice =>4
+  PID TTY          TIME CMD
+ 5639 pts/0    00:00:00 bash
+ 5808 pts/0    00:00:00 p1
+ 5812 pts/0    00:00:00 sh
+ 5813 pts/0    00:00:00 ps
+
+
+********************************Menu***************************
+1.fork 
+2.exec 
+3.wait 
+4.ps 
+5.joint 
+6.exit
+Enter choice =>5
+join: f1.txt: No such file or directory
+
+
+********************************Menu***************************
+1.fork 
+2.exec 
+3.wait 
+4.ps 
+5.joint 
+6.exit
+Enter choice =>6
+*/
 
 
 
